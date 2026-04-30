ae3670952fe9d5f40583e4ed6cb79c175c2344b1
ChenJianAgent <josephchenjian@outlook.com>
2026-04-30 23:54:20 +0000

    Improve bot logging output

--- bot.py
+++ bot.py
@@ -4,10 +4,19 @@ import discord
 
 class Client(discord.Client):
     async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+        print(f'[ready] user={self.user} user_id={self.user.id}')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        channel_name = getattr(message.channel, 'name', str(message.channel))
+        guild_name = getattr(message.guild, 'name', 'DM')
+        print(
+            f'[seen] author={message.author} '
+            f'author_id={message.author.id} '
+            f'is_bot={message.author.bot} '
+            f'guild={guild_name} '
+            f'channel={channel_name} '
+            f'content={message.content!r}'
+        )
 
 
 intents = discord.Intents.default()
